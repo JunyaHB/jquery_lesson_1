30431080728a0add6aff3df430db5b84279cb6e5
JunyaHB <a1933jh@aiit.ac.jp>
2019-08-23 18:19:51 +0900

    complete

---
+++ dist/script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+$(function () {
+  // 「#login-show」要素に対するclickイベントを作成してください
+  $('#login-show').click(function () {
+    $('#login-modal').fadeIn();
+  });
+  $('.signup-show').click(function () {
+    $('#signup-modal').fadeIn();
+  });
+  $('.close-modal').click(function () {
+    $('#signup-modal').fadeOut();
+    $('#login-modal').fadeOut();
+  });
+  $('.lesson-hover').hover(function () {
+    $(this).find(".text-contents").addClass('text-active');
+  }, function () {
+    $(this).find(".text-contents").removeClass('text-active');
+  });
+  $('.faq-list-item').click(function () {
+    var $answer = $(this).find('.answer');
+
+    if ($answer.hasClass('open')) {
+      $answer.removeClass('open');
+      $answer.slideUp();
+      $(this).find('span').text('+');
+    } else {
+      $answer.addClass('open');
+      $answer.slideDown();
+      $(this).find('span').text('-');
+    }
+  });
+});
